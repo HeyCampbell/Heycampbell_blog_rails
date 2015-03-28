10c039a933560c59206b086e301857ff6c9b0a64
heycampbell <heycampbell@gmail.com>
2015-03-28 17:30:45 -0400

    add markdown converter and method to load md files into db

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @projects = Project.all
-
+    @thoughts = Thought.take(5)
   end
 
   def signin
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,2 +1,25 @@
 class Thought < ActiveRecord::Base
+
+  def clean_body
+    ActionController::Base.helpers.strip_tags(self.body).gsub("\n", ' ')
+  end
+
+  def self.md_to_db
+    blog_files = Thought.get_md_files
+    blog_files.each do |blog|
+      thought = Thought.parse_md(blog)
+      unless Thought.find_by(title: thought[:title])
+        Thought.create(thought)
+      end
+    end
+  end
+
+  def self.get_md_files
+    Dir.glob("lib/blogposts/*.md")
+  end
+
+  def self.parse_md(filename)
+    data = Metadown.render(File.open(filename, 'rb').read)
+    {title: data.metadata["title"], thumbnail: data.metadata["thumbnail"], body: data.output}
+  end
 end
