6dfe930c87f2498958fbabd9f35edb678fba6db9
heycampbell <heycampbell@gmail.com>
2015-04-02 17:50:26 -0400

    clean up style sheets for style guide

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-  $( ".menu-burger" ).click(function(target) {
+  $( ".sidenav" ).on('click', '.menu-burger', function(target) {
     $(this).parent().toggleClass("expand");
-    // alert( "Handler for .click() called." );
   });
-
 });
